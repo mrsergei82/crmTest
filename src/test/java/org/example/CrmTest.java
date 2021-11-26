10b8f7a6897412f166e865c951a4fa1eca96c497
Sergei <mr.sergei82@mail.ru>
2021-11-26 20:05:31 +0300

    проверки логина и создания проекта

--- src/test/java/org/example/CrmTest.java
+++ src/test/java/org/example/CrmTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.BooleanSupplier;
 
 public class CrmTest {
     @Test
@@ -21,10 +23,14 @@ public class CrmTest {
         driver.findElement(By.id("prependedInput")).sendKeys(" Applanatest1");
         driver.findElement(By.id("prependedInput2")).sendKeys("Student2020!");
         driver.findElement(By.id("_submit")).click();
+        Assertions.assertFalse(driver.getTitle()=="Логин");
+
         driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[@class=\"dropdown\"]/a[@class=\"unclickable\"]/span"))
                 .click();
         driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/ul/li[4]/a/span")).click();
         driver.findElement(By.xpath("//a[@title=\"Создать проект\"]")).click();
+        System.out.println(driver.getCurrentUrl());
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://crm.geekbrains.space/project/create/"));
         driver.findElement(By.name("crm_project[name]")).sendKeys("TestP");
         driver.findElement(By.xpath("//a[@class=\"select2-choice select2-default\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]/div")).click();
@@ -37,6 +43,8 @@ public class CrmTest {
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/ul[2]/li[1]/div")).click();
         driver.findElement(By.xpath("//button[@class=\"btn btn-success action-button\"]")).click();
         driver.findElement(By.xpath("//div[@class='filter-container']/span/div[1]/div[1]")).click();
+
+        Assertions.assertTrue(driver.getCurrentUrl().equals("https://crm.geekbrains.space/project/"));
         driver.findElement(By.xpath("//div[@class='filter-container']/span/div[1]/div[2]/div/div/button")).click();
         driver.findElement(By.xpath("//div[@class='filter-container']/span/div/div[2]/div/div/ul/li[3]/a")).click();
         driver.findElement(By.xpath("//div[1]/div/div/span/div[1]/div[2]/div/div[2]/input[1]")).sendKeys("TestP");
